9b1ade54866d28143910750c942cca40ab6da96e
Luca Rizzello <naganack@gmail.com>
2018-12-22 18:07:30 +0100

    Initial Commit

---
+++ Warri0r/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Warri0r
+//
+//  Created by Luca on 22.12.18.
+//  Copyright © 2018 Luca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Warri0r/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Warri0r
+//
+//  Created by Luca on 22.12.18.
+//  Copyright © 2018 Luca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
